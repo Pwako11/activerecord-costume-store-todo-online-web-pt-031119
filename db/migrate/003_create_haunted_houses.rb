981215b60771b28917049c1b5b8d6306ef31a9d2
Patrick A <akolo@hotmail.com>
2019-08-05 04:15:58 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class HauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :HauntedHouses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.boolean :familyfriend
+      t.datetime :openingdate
+      t.datetime :closingdate
+      t.text :longdescription 
+    end
+  end
+end
